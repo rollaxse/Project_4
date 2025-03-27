7a54147c4b944a5c44ee0d08509d7cd2697d3f35
Mason <rollaxse@kali.gig>
2025-03-26 21:36:27 -0500

    ya

--- master_password.py
+++ master_password.py
@@ -1,17 +1,19 @@
 from hashlib import sha256
-from Cryptodome.Cipher import AES 
-from pbkdf2 import PBKDF2
+from Crypto.Cipher import AES 
+from Crypto.Protocol.KDF import PBKDF2 
 import hashlib
 from base64 import b64encode, b64decode
 
 # Enter salt here in ******* field. Enter binary string.
-salt = b'********'
+salt = b'\xa2\x93\xb8\xb0\x0f\x9e\xb4\xae\xd1\xfd\xc0Z\xb2\xf0\xa1b'
+
+
 
 
 def query_master_pwd(master_password, second_FA_location): 
 
     # Enter password hash in ******** field. Use PBKDF2 and Salt from above. Use master_password_hash_generator.py to generate a master password hash.
-    master_password_hash = "********"
+    master_password_hash = "7fe2d7c9b42608c8b3cf36e430370228b780621e4b3ae9c78507c54873dc4ec1"
 
     compile_factor_together = hashlib.sha256(master_password + second_FA_location).hexdigest()
 
